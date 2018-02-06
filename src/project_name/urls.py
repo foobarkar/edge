8efd02a03d81a455233f8c0f473b276c4daf8f1b
Joseph Dane <Joseph_Dane@symantec.com>
2018-02-05 21:39:36 -0500

    Changes to support Django 2

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,11 +7,11 @@ import accounts.urls
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.HomePage.as_view(), name='home'),
-    url(r'^about/$', views.AboutPage.as_view(), name='about'),
-    url(r'^users/', include(profiles.urls, namespace='profiles')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(accounts.urls, namespace='accounts')),
+    path(r'', views.HomePage.as_view(), name='home'),
+    path(r'about/', views.AboutPage.as_view(), name='about'),
+    path(r'users/', include(profiles.urls, namespace='profiles')),
+    path(r'admin/', admin.site.urls),
+    path(r'', include(accounts.urls, namespace='accounts')),
 ]
 
 # User-uploaded files like profile pics need to be served in development
@@ -21,5 +21,5 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path(r'__debug__/', include(debug_toolbar.urls)),
     ]
